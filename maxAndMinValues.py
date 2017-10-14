8ffaa80543f34cead4eb1ee26d9a9fbe98990e08
Jillo Abdullahi <jillo.abdulla@gmail.com>
2017-10-14 12:40:44 +0300

    [Update] Slight modification made

--- maxAndMinValues.py
+++ maxAndMinValues.py
@@ -3,3 +3,8 @@ def findMaxMin(list):
 	if min(list) == max(list):
 		return [min(list)]
 	return [min(list),max(list)]
+
+myList = [3,3,4,6,5,6,6]
+
+if __name__ == "__main__":
+	print (findMaxMin (myList))
